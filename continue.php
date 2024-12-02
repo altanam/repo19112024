b31b9b8c58a4dc19501f0c048b1d735fddc7e5cc
Altana <@almosorova@yandex.ru>
2024-12-02 22:08:49 +0800

    change code

--- continue.php
+++ continue.php
@@ -1,7 +1,3 @@
-This is file frome Remote Repository
-1 2 3 4 5 6 7 8 9
-9 8 7 6 5 4 3 2 1
-=======
-I`m write here working site code
+I`m write here working site code - conflict from Dev1
 here mistake code
 
